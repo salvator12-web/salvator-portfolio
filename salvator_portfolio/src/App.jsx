247afab732bfbaf7703303e0f8f54fe33b698427
salvator12-web <salvatorahishakiye@gmail.com>
2026-07-18 12:51:13 +0200

    Add files via upload

--- salvator_portfolio/src/App.jsx
+++ salvator_portfolio/src/App.jsx
@@ -1,1 +1,37 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import NotFound from "./pages/NotFound";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import AboutMe from "./components/AboutMe";
+import ContactMe from "./components/ContactMe";
+import Skills from "./components/Skills";
 
+const App = () => {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        {/* Navbar */}
+        <NavBar />
+
+        {/* Main Content Area */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutMe />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<ContactMe />} />
+            <Route path="/skills" element={<Skills />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+
+        {/* Footer (Stays at the bottom) */}
+        <Footer />
+      </div>
+    </Router>
+  );
+};
+
+export default App;
